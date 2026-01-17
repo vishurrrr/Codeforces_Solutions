27bec2eff630497f8edc9285a25955e8f90c9d1f
Vaishalirwt <vaishalirawattt@gmail.com>
2026-01-17 17:49:45 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>#include<string>using namespace std; int main(){int n;cin >> n; while(n--){string word;cin>>word; if(word.length()<=10){cout<<word<<endl;}else{cout<<word[0]<<word.length()-2<<word[word.length()-1]<<endl;}}return 0;}
